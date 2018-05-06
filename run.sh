c2d7b233a5817b56055b0b26a817382589dd013f
Matt Dowds <matthewd99@gmail.com>
2018-05-06 18:50:23 +0100

    Correct shebang and params in run script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 python update_journey_times.py /app/data/ccdb.sqlite -k $GMAPS_API_KEY;
-python update_season_tickets.py /app/data/ccdb.sqlite -k $GMAPS_API_KEY;
+python update_season_tickets.py /app/data/ccdb.sqlite;
