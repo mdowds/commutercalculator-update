4971ae528e8df9ec2d43124151a15c00b1716d98
Matt Dowds <matthewd99@gmail.com>
2019-01-04 18:50:25 +0000

    Create main file as entry point

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import updaters
 from interfaces.database import Database
 from updaters import JourneyTimesInteractor
 
-if __name__ == '__main__':
+
+def update_journey_times():
     database = Database(firestore.Client())
     interactor = JourneyTimesInteractor(db=database, api_key=os.environ['GMAPS_API_KEY'])
 
@@ -16,3 +17,7 @@ if __name__ == '__main__':
 
     if update_results.has_errors:
         sys.exit(1)
+
+
+if __name__ == '__main__':
+    update_journey_times()
