927c4218a71678a1fa60b67bfa7c958e4048fbcf
Matt Dowds <matthewd99@gmail.com>
2019-01-11 12:57:18 +0000

    Update test script to try saving journey costs as well

--- single_update.py
+++ single_update.py
@@ -5,15 +5,20 @@ from google.cloud import firestore
 from interfaces.database import Database
 from models import Station
 from updaters import JourneyTimesInteractor
+from updaters.journey_costs_interactor import JourneyCostsInteractor
 
 if __name__ == '__main__':
     firestore = firestore.Client()
     database = Database(firestore)
-    interactor = JourneyTimesInteractor(db=database, api_key=os.environ['GMAPS_API_KEY'])
+    chx = Station.from_dict(firestore.collection('destinations').document('CHX').get().to_dict())
+    lew = Station.from_dict(firestore.collection('stations').document('LEW').get().to_dict())
 
-    bank = Station.from_dict(firestore.collection('destinations').document('BAH').get().to_dict())
-    kgx = Station.from_dict(firestore.collection('destinations').document('KGX').get().to_dict())
+    time_interactor = JourneyTimesInteractor(db=database, api_key=os.environ['GMAPS_API_KEY'])
+    time = time_interactor.get_update(chx, lew)
+    time_interactor.save_update(chx, lew, time.value)
 
-    time = interactor.get_update(bank, kgx)
-    interactor.save_update(bank, kgx, time.value)
-    print("Updated Bank to King's Cross")
+    costs_interactor = JourneyCostsInteractor(db=database)
+    costs = costs_interactor.get_update(chx, lew)
+    costs_interactor.save_update(chx, lew, costs.value)
+
+    print("Updated Lewisham to Charing Cross")
