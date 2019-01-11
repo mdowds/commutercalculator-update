ec90da8383867b746616e7a72b3d279015daf995
Matt Dowds <matthewd99@gmail.com>
2019-01-11 12:58:40 +0000

    Add updating journey costs to main script

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from google.oauth2 import service_account
 import updaters
 from interfaces.database import Database
 from updaters import JourneyTimesInteractor
+from updaters.journey_costs_interactor import JourneyCostsInteractor
 
 
 def update_journey_times():
@@ -25,5 +26,20 @@ def update_journey_times():
         sys.exit(1)
 
 
+def update_journey_costs():
+    key = json.loads(os.environ['GCLOUD_SA_KEY'])
+    credentials = service_account.Credentials.from_service_account_info(key)
+
+    database = Database(firestore.Client(project=os.environ['GCLOUD_PROJECT_ID'], credentials=credentials))
+    interactor = JourneyCostsInteractor(db=database)
+
+    update_results = updaters.update(interactor)
+    print("Journey costs: " + update_results.message)
+
+    if update_results.has_errors:
+        sys.exit(1)
+
+
 if __name__ == '__main__':
     update_journey_times()
+    update_journey_costs()
